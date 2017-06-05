beac0298e5bb4759aea1540e9f92563f5ba742ec
davidrfreeman <davidfreeman@holos.solutions>
2017-06-04 22:01:36 -0600

    Add Ip request

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
+"use strict";
+
 // server.js
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+let PORT = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
-app.get("api/httpheaders", (req, res) => {
-  res.json({res: "It's Alive!!!"});
+app.get("/api/httpheaders", (req, res) => {
+
+  let ipAddress = req.ip;
+  let software = req.headers['user-agent'];
+
+  res.json({software: software, ip: ipAddress});
+
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(PORT, () => {
+  console.log('Your app is listening on port', PORT);
 });
